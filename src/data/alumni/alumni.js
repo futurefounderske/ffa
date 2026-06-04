12588ad7cb3f1c699da93e40239d69603ab2ffca
PracticalSecret <murigipeninah1@gmail.com>
2026-06-04 21:03:43 +0300

    fixed sth in alumni

--- src/data/alumni/alumni.js
+++ src/data/alumni/alumni.js
@@ -7,7 +7,7 @@ const alumni = [
         src: video1,
         cohort: "2025 - Cohort 1",
         location: "Nairobi, Kenya",
-        bio: "Founded by a teen graduate, the teen pop-up brings both teenagers and young adults together to put their items on display, availing them to the public, making their passions both engaging and profitable.",
+        bio: "Founded by a teen participant, the teen pop-up brings both teenagers and young adults together to put their items on display, availing them to the public, making their passions both engaging and profitable.",
         whatsapp: "#",
         instagram: "#",
         website: "#",
